ec56b9adfd24116a3c23fe5b7ac277bb20938d0f
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-11-27 22:17:29 +0545

    practice done

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+document.addEventListener("mousemove", (dets) => {
+  document.body.style.setProperty("--x", dets.clientX + "px");
+  document.body.style.setProperty("--y", dets.clientY + "px");
+});
 
+const h1 = document.querySelector("h1");
+const text = h1.innerText;
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-document.addEventListener("mousemove",(dets)=>{
+let iteration = 0;
 
-    document.body.style.setProperty("--x",dets.clientX + "px")
-    document.body.style.setProperty("--y",dets.clientY + "px")
+function newText() {
+  const str = text.split("").map((char, idx) => {
 
-})
+    if(idx < iteration){
+        return char;
+    }
+      return characters.split("")[Math.floor(Math.random() * 52)];
+    })
+    .join("");
+
+  h1.innerHTML = str;
+  iteration += 0.2
+}
+
+setInterval(newText, 30)
