47e87cf361e8ef9d786eaca0119549c2d9f75908
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-11-27 09:20:21 +0545

    effect done

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener("mousemove",(dets)=>{
+
+    document.body.style.setProperty("--x",dets.clientX + "px")
+    document.body.style.setProperty("--y",dets.clientY + "px")
+
+})
